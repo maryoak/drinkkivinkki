16d29045dd7ddfaad850fa2b6f744a6da650ac31
Hannamari Tamminen <hannamari.tamminen@helsinki.fi>
2014-03-13 15:56:55 +0200

    Netbeans-projekti gittiin

---
+++ drinkkivinkki/src/main/java/drinkkivinkki/drinkkivinkki/App.java
@@ -0,0 +1,13 @@
+package drinkkivinkki.drinkkivinkki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
